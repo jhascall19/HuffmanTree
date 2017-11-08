2c37e73f98555cd72b0ec223f8e2fd3b5e9384a8
joshyhasc <jhascall19@Jhascall19.local>
2017-11-08 14:10:38 -0700

    Fixed Stuff

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -6,22 +6,22 @@ import java.util.ArrayList;
 
 public class  HuffmanTree {
     private
-    HuffmanTree(String a){
-       ArrayList chars = new ArrayList();
-       ArrayList count = new ArrayList();
+    HuffmanTree(String a) {
+        ArrayList chars = new ArrayList();
+        ArrayList <Integer> count = new ArrayList();
         int counter = 0;
 
-        for (int i = 0; i <a.length()-1 ; i++) {
-            chars.add(a.charAt(i));
-            for (int q = 0; q < a.length()-1 ; q++) {
-                if ((char)chars.get(i) == a.charAt(q)){
-                    counter ++;
-                }
-            }
-        count.add(counter);
-            counter = 0;
-        }
+        for (int i = 0; i < a.length() - 1; i++) {
 
+            if (chars.contains(a.charAt(i))){
+                int loc =  chars.indexOf(a.charAt(i));
+                count.set(loc , count.get(loc)+1);
+            }else {
+                chars.add(a.charAt(i));
+                count.add(1);
+            }
+
+        }
     }
 
 
