71b7f7bea68678055993aff6313a4d79c00990d6
QVoboril18 <qvoboril18@kentdenver.org>
2017-11-08 13:33:27 -0700

    why u do this to me

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+
 /**
  * Created by jhascall19 on 11/7/17.
  */
-public class HuffmanTree {
 
-    public HuffmanTree(){
+public class  HuffmanTree {
+    private
+    HuffmanTree(String a){
+        ArrayList cars= new ArrayList();
+        ArrayList target = new ArrayList();
+        for (int i = 0; i < a.length(); i++) {
+            cars.add(a.charAt(i));
+        }
+        target.add(cars.get(0));
+        int counter=0;
+        int length = cars.size();
 
-    }
+        for (int i = 0; i <cars.size() ; i++) {
 
-    public boolean isLeaf(){
-        boolean t = false;
-        return t;
+            if (cars.get(i).equals(target)){
+                cars.remove(i);
+                counter++;
+            }else {
+                target = (char) cars.get(i);
+
+            }
+
+
+        }
     }
 
 
 }
+
