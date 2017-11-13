b2e56acce67a51b3ae0191b23b5fdf228a008326
QVoboril18 <qvoboril18@kentdenver.org>
2017-11-13 09:31:00 -0700

    comments

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -9,6 +9,8 @@ import java.util.PriorityQueue;
 
 public class  HuffmanTree  {
     HuffmanTree(String a) {
+
+        // making 2 parrallel arraylists that contain # time appeared and character (Lines 14-25)
         ArrayList chars = new ArrayList();
         ArrayList<Integer> count = new ArrayList();
         for (int i = 0; i < a.length() - 1; i++) {
@@ -21,21 +23,21 @@ public class  HuffmanTree  {
                 count.add(1);
             }
         }
+        // Priority Queue (lines 27-31)
         Node n;
         PriorityQueue queue = new PriorityQueue();
 
         for (int i = 0; i <chars.size() ; i++) {
          n = new Node<Integer, Character>(count.get(i),(Character)chars.get(i));
          queue.add(n);
-
-
         }
+        // feeding priority queue into tree format (Lines 33- )
         for (int q = 0; q < queue.size(); q++) {
 
         }
 
 
-                         }
+    }
 
 
 }
